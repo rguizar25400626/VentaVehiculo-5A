8ec889de2680b758bc1705df002bd1d5eccc1d0e
rguizar25600626 <roanguizarlo@ittepic.com>
2026-02-17 10:10:58 -0700

    getters y setters

--- proyecto-VentaVehiculo-5A/src/Cliente.java
+++ proyecto-VentaVehiculo-5A/src/Cliente.java
@@ -1,25 +1,56 @@
+
 /**
  *
  * @author Nappe
  */
 public class Cliente {
-    
+
     private String rs;
     private String rfc;
     private String email;
-    
-    public Cliente(String rs, String rfc, String email){
-    
+
+    public Cliente(String rs, String rfc, String email) {
+
         this.rs = rs;
         this.rfc = rfc;
         this.email = email;
-            
+
     }
-    
+
     @Override
-    public String toString(){
+    public String toString() {
         return "Razón Social: " + this.rs
-            + "\nRFC: " + this.rfc
-            + "\nE-mail: " + this.email;
+                + "\nRFC: " + this.rfc
+                + "\nE-mail: " + this.email;
+    }
+
+    public String razonSocial() {
+
+        return this.rs;
+    }
+
+    public String rfc() {
+
+        return this.rfc;
+    }
+
+    public String email() {
+
+        return this.email;
+    }
+
+    public void setRazonSocial(String nrs) {
+        this.rs = nrs;
+
+    }
+
+    public void setRfc(String nrfc) {
+        this.rfc = nrfc;
+
+    }
+
+    public void setEmail(String nemail) {
+        this.email = nemail;
+
     }
 }
--- proyecto-VentaVehiculo-5A/src/Vehiculo.java
+++ proyecto-VentaVehiculo-5A/src/Vehiculo.java
@@ -1,27 +1,52 @@
+
 /**
  *
  * @author Nappe
  */
 
 public class Vehiculo {
-    
+
     private String marca;
     private double precio;
     private String motor;
-    
-    public Vehiculo(String marca, double precio, String motor){
-    
+
+    public Vehiculo(String marca, double precio, String motor) {
+
         this.marca = marca;
         this.precio = precio;
         this.motor = motor;
-            
+
     }
-    
+
     @Override
-    public String toString(){
+    public String toString() {
         return "Marca: " + this.marca
-            + "\nPrecio: " + this.precio
-            + "\nMotor: " + this.motor;
+                + "\nPrecio: " + this.precio
+                + "\nMotor: " + this.motor;
     }
-    
+
+    public String getMarca() {
+        return this.marca;
+    }
+
+    public double getPrecio() {
+        return this.precio;
+    }
+
+    public String getMotot() {
+        return this.motor;
+    }
+
+    public void setMarca(String nmarca) {
+        this.marca = nmarca;
+    }
+
+    public void setPrecio(double nprecio) {
+        this.precio = nprecio;
+    }
+
+    public void setMotor(String nmotor) {
+        this.motor = nmotor;
+    }
+
 }
